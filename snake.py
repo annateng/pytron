cdf5272f29837ccd2c6c0323f1da8b1ba18241d4
Anna Teng <anna.w.teng@gmail.com>
2020-05-26 19:03:01 -0400

    working game with 4 players

--- snake.py
+++ snake.py
@@ -8,8 +8,9 @@ class Player:
     y = 20 
     xs = deque()
     ys = deque()
+    color = (255,255,255)
 
-    def __init__(self, x_start=20, y_start=20, start_direction="RIGHT"):
+    def __init__(self, x_start=20, y_start=20, start_direction="RIGHT", color=(255,255,255)):
         self._X_START = x_start
         self._Y_START = y_start
         self._START_DIRECTION = start_direction
@@ -18,6 +19,9 @@ class Player:
 
         self.x = x_start
         self.y = y_start
+        self.xs = deque()
+        self.ys = deque()
+        self.color = color
 
     def moveRight(self):
         self._direction = "RIGHT"
@@ -43,19 +47,12 @@ class Player:
         if self._direction == "DOWN":
             self.y = self.y + self._bSize
 
-    def reset(self):
-        self.x = self._X_START
-        self.y = self._Y_START
-        self.xs = deque()
-        self.ys = deque()
-        self._direction = self._START_DIRECTION 
-
 class App:
 
     windowHeight = 1600
     windowWidth = 3000
-    n_players = 2
-    players = 0
+    players = []
+    n_players = 1
 
     # COLOR CONSTANTS
     BLACK = (0,0,0)
@@ -66,34 +63,34 @@ class App:
     RED = (255,0,0)
     DARKTEAL = (27,70,65)
     DARKTEALHOVER = (55,84,76)
+    LIMEGREEN = (143,250,0)
+    MAUVE = (216,203,207)
+    PINK = (49,181,252)
 
     def __init__(self):
         self._running = True
         self._game_over = False
         self._display_surf = True
 
-        player1 = Player(20, 20, "RIGHT")
-        player2 = Player(self.windowWidth - 40, 20, "LEFT")
-        self.players = [player1, player2]
-
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode((self.windowWidth, self.windowHeight), pygame.HWSURFACE)
         self._running = True
         self._game_over = False
+        self._newgame = True
 
     def on_event(self, event):
         if event.type == QUIT:
             self._running = False
 
     def on_loop(self):
-        if not self._game_over: 
+        if not self._newgame and not self._game_over: 
             for player in self.players: 
                 player.addBlock()
                 # collision detection: wall
                 if player.x < 0 or player.y < 0 or player.y + 20 > self.windowHeight or player.x + 20 > self.windowWidth:
                     self._game_over = True
-                # collision detection: self
+                # collision detection: other snakes
                 for coord in zip(player.xs, player.ys):
                     for inner_player in self.players: 
                         if coord == (inner_player.x, inner_player.y): self._game_over = True
@@ -101,10 +98,13 @@ class App:
     def on_render(self):
         self._display_surf.fill(self.BLACK)
         
-        for player in self.players:
-            for coord in zip(player.xs, player.ys):
-                pygame.draw.rect(self._display_surf, self.YELLOW, (coord[0], coord[1], 20, 20), 0)
-            pygame.draw.rect(self._display_surf, self.YELLOW, (player.x, player.y, 20, 20), 0)
+        if self._newgame:  
+            self._draw_newgame()
+        else:
+            for player in self.players:
+                for coord in zip(player.xs, player.ys):
+                    pygame.draw.rect(self._display_surf, player.color, (coord[0], coord[1], 20, 20), 0)
+                pygame.draw.rect(self._display_surf, player.color, (player.x, player.y, 20, 20), 0)
         
         if self._game_over: self._draw_gameover()
         pygame.display.flip()
@@ -113,9 +113,9 @@ class App:
         pygame.quit()
 
     def on_reset(self):
-        for player in self.players:
-            player.reset()
+        self.players = []
         self._game_over = False
+        self._newgame = True
 
     def on_execute(self):
         if self.on_init() == False:
@@ -129,10 +129,29 @@ class App:
             keys = pygame.key.get_pressed()
             if keys[K_ESCAPE]: self._running = False
 
-            if keys[K_RIGHT]: self.players[1].moveRight()
-            if keys[K_LEFT]: self.players[1].moveLeft()
-            if keys[K_UP]: self.players[1].moveUp()
-            if keys[K_DOWN]: self.players[1].moveDown()
+            # PLAYER 1: ARROW KEYS
+            if keys[K_RIGHT]: self.players[0].moveRight()
+            if keys[K_LEFT]: self.players[0].moveLeft()
+            if keys[K_UP]: self.players[0].moveUp()
+            if keys[K_DOWN]: self.players[0].moveDown()
+            # PLAYER 2: WASD
+            if self.n_players > 1:
+                if keys[K_d]: self.players[1].moveRight()
+                if keys[K_a]: self.players[1].moveLeft()
+                if keys[K_w]: self.players[1].moveUp()
+                if keys[K_s]: self.players[1].moveDown()
+            # PLAYER 3: GVBN
+            if self.n_players > 2:
+                if keys[K_n]: self.players[2].moveRight()
+                if keys[K_v]: self.players[2].moveLeft()
+                if keys[K_g]: self.players[2].moveUp()
+                if keys[K_b]: self.players[2].moveDown()
+            # PLAYER 4: OKL;
+            if self.n_players > 3:
+                if keys[K_SEMICOLON]: self.players[3].moveRight()
+                if keys[K_k]: self.players[3].moveLeft()
+                if keys[K_o]: self.players[3].moveUp()
+                if keys[K_l]: self.players[3].moveDown()
 
             self.on_loop()
             self.on_render()
@@ -140,8 +159,47 @@ class App:
 
         self.on_cleanup()
 
-    def set_running_false(self):
+    def set_running_false(self): 
         self._running = False
+
+    def set_oneplayer(self): 
+        self.n_players = 1
+        player1 = Player(int(self.windowWidth/2), int(self.windowHeight/2), "RIGHT", self.YELLOW)
+        self.players = [player1,]
+        self._newgame = False
+    def set_twoplayer(self): 
+        self.n_players = 2
+        player1 = Player(20, int(self.windowHeight/2), "RIGHT", self.YELLOW)
+        player2 = Player(self.windowWidth - 40, int(self.windowHeight/2), "LEFT", self.MAUVE)
+        self.players = [player1, player2]
+        self._newgame = False
+    def set_threeplayer(self): 
+        self.n_players = 3
+        player1 = Player(20, 500, "RIGHT", self.YELLOW)
+        player2 = Player(self.windowWidth - 40, 500, "LEFT", self.MAUVE)
+        player3 = Player(int(self.windowWidth/2), 1100, "UP", self.PINK)
+        self.players = [player1, player2, player3]
+        self._newgame = False
+    def set_fourplayer(self): 
+        self.n_players = 4
+        player1 = Player(20, 500, "RIGHT", self.YELLOW)
+        player2 = Player(self.windowWidth - 40, 500, "LEFT", self.MAUVE)
+        player3 = Player(20, 1100, "RIGHT", self.PINK)
+        player4 = Player(self.windowWidth - 40, 1100, "LEFT", self.WHITE)
+        self.players = [player1, player2, player3, player4]
+        self._newgame = False
+    
+    def _draw_newgame(self):
+        font = pygame.font.Font(None, 250)
+        text = font.render("New Game", True, self.LIMEGREEN)
+        text_rect = text.get_rect()
+        text_x = int(self.windowWidth / 2 - text_rect.width / 2)
+        text_y = int(self.windowHeight / 2 - text_rect.height / 2 - 200)
+        self._display_surf.blit(text, [text_x, text_y])
+        self._draw_button("Play One Player", self.DARKTEAL, self.DARKTEALHOVER, self.set_oneplayer, 100, 1200, 400, 100)
+        self._draw_button("Play Two Player", self.DARKTEAL, self.DARKTEALHOVER, self.set_twoplayer, 100, 1400, 400, 100)
+        self._draw_button("Play Three Player", self.DARKTEAL, self.DARKTEALHOVER, self.set_threeplayer, self.windowWidth - 500, 1200, 400, 100)
+        self._draw_button("Play Four Player", self.DARKTEAL, self.DARKTEALHOVER, self.set_fourplayer, self.windowWidth - 500, 1400, 400, 100)
 
     def _draw_gameover(self):
         font = pygame.font.Font(None, 250)
