9b7386be4559cc1ca123d510aced34e4bb8efc85
Anna Teng <anna.w.teng@gmail.com>
2020-05-27 22:28:46 -0400

    refactored to add App._state. fixed bug with score reset between games

--- bmtron.py
+++ bmtron.py
@@ -1,5 +1,6 @@
 from collections import deque
 from pygame.locals import *
+from enum import Enum, auto
 import pygame
 import time
 
@@ -50,16 +51,21 @@ class Player:
 
 class App:
 
+    class State(Enum):
+        NEWGAME = auto()
+        ROUNDOVER = auto()
+        PLAYING = auto()
+
     windowHeight = 1600
     windowWidth = 3000
     players = []
-    n_players = 1
+    n_players = 0
     scores = []
     winners = []
 
     # COLOR CONSTANTS
     BLACK = (0,0,0)
-    YELLOW = (255,255,0)
+    YELLOW = (255,255,150)
     WHITE = (255,255,255)
     DARKRED = (153,0,76)
     DARKREDHOVER = (169,0,102)
@@ -71,25 +77,23 @@ class App:
     PINK = (249,181,252)
     LIGHTBLUE = (87,178,220)
     SAND = (231,174,121)
+    GREEN = (69,111,28)
 
     def __init__(self):
-        self._running = True
-        self._game_over = False
         self._display_surf = True
 
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode((self.windowWidth, self.windowHeight), pygame.HWSURFACE)
         self._running = True
-        self._game_over = False
-        self._newgame = True
+        self._state = self.State.NEWGAME
 
     def on_event(self, event):
         if event.type == QUIT:
             self._running = False
 
     def on_loop(self):
-        if not self._newgame and not self._game_over: 
+        if self._state == self.State.PLAYING: 
             for i in range(self.n_players):
                 if self.players[i].alive: 
                     self.players[i].addBlock()
@@ -106,15 +110,15 @@ class App:
             winner_count = self.n_players 
             for i in range(self.n_players):
                 if self.winners[i] == 0: winner_count -= 1
-                if winner_count < 2: self._game_over = True
-            if self._game_over: 
+                if winner_count < 2: self._state = self.State.ROUNDOVER
+            if self._state == self.State.ROUNDOVER: 
                 for i in range(self.n_players):
                     if self.winners[i] == 1: self.scores[i] += 1
 
     def on_render(self):
         self._display_surf.fill(self.BLACK)
         
-        if self._newgame:  
+        if self._state == self.State.NEWGAME:  
             self._draw_newgame()
         else:
             for player in self.players:
@@ -122,7 +126,7 @@ class App:
                     pygame.draw.rect(self._display_surf, player.color, (coord[0], coord[1], 20, 20), 0)
                 pygame.draw.rect(self._display_surf, player.color, (player.x, player.y, 20, 20), 0)
         
-        if self._game_over: self._draw_gameover()
+        if self._state == self.State.ROUNDOVER: self._draw_gameover()
         pygame.display.flip()
 
     def on_cleanup(self): 
@@ -135,7 +139,11 @@ class App:
         elif self.n_players == 2: self.set_twoplayer()
         elif self.n_players == 3: self.set_threeplayer()
         elif self.n_players == 4: self.set_fourplayer()
-        self._game_over = False
+        self._state = self.State.PLAYING
+
+    def on_newgame(self):
+        self._state = self.State.NEWGAME
+        self.scores = []
 
     def on_execute(self):
         if self.on_init() == False:
@@ -149,10 +157,11 @@ class App:
             if keys[K_ESCAPE]: self._running = False
 
             # PLAYER 1: ARROW KEYS
-            if keys[K_RIGHT]: self.players[0].moveRight()
-            if keys[K_LEFT]: self.players[0].moveLeft()
-            if keys[K_UP]: self.players[0].moveUp()
-            if keys[K_DOWN]: self.players[0].moveDown()
+            if self.n_players > 0:
+                if keys[K_RIGHT]: self.players[0].moveRight()
+                if keys[K_LEFT]: self.players[0].moveLeft()
+                if keys[K_UP]: self.players[0].moveUp()
+                if keys[K_DOWN]: self.players[0].moveDown()
             # PLAYER 2: WASD
             if self.n_players > 1:
                 if keys[K_d]: self.players[1].moveRight()
@@ -174,16 +183,9 @@ class App:
 
             self.on_loop()
             self.on_render()
-            time.sleep(1/30)
+            time.sleep(1/60)
 
         self.on_cleanup()
-
-    def set_running_false(self): 
-        self._running = False
-
-    def set_newgame_true(self):
-        self._game_over = False
-        self._newgame = True
 
     def set_oneplayer(self): 
         self.n_players = 1
@@ -191,7 +193,7 @@ class App:
         self.players = [player1,]
         if len(self.scores) == 0: self.scores = [0,]
         self.winners = [1,]
-        self._newgame = False
+        self._state = self.State.PLAYING
     def set_twoplayer(self): 
         self.n_players = 2
         player1 = Player(20, int(self.windowHeight/2), "RIGHT", self.YELLOW)
@@ -199,7 +201,7 @@ class App:
         self.players = [player1, player2]
         if len(self.scores) == 0: self.scores = [0,0]
         self.winners = [1,1]
-        self._newgame = False
+        self._state = self.State.PLAYING
     def set_threeplayer(self): 
         self.n_players = 3
         player1 = Player(20, 500, "RIGHT", self.YELLOW)
@@ -208,7 +210,7 @@ class App:
         self.players = [player1, player2, player3]
         if len(self.scores) == 0: self.scores = [0,0,0]
         self.winners = [1,1,1]
-        self._newgame = False
+        self._state = self.State.PLAYING
     def set_fourplayer(self): 
         self.n_players = 4
         player1 = Player(20, 500, "RIGHT", self.YELLOW)
@@ -218,7 +220,7 @@ class App:
         self.players = [player1, player2, player3, player4]
         if len(self.scores) == 0: self.scores = [0,0,0,0]
         self.winners = [1,1,1,1]
-        self._newgame = False
+        self._state = self.State.PLAYING
     
     def _draw_newgame(self):
         font = pygame.font.Font(None, 250)
@@ -233,36 +235,23 @@ class App:
         self._draw_button("Play Four Player", self.DARKTEAL, self.DARKTEALHOVER, self.set_fourplayer, self.windowWidth - 500, 1400, 400, 100)
 
     def _draw_gameover(self):
-        font = pygame.font.Font(None, 250)
-        text = font.render("Game Over", True, self.WHITE)
-        text_rect = text.get_rect()
-        text_x = int(self.windowWidth / 2 - text_rect.width / 2)
-        text_y = int(self.windowHeight / 2 - text_rect.height / 2 - 200)
-        pygame.draw.rect(self._display_surf, self.RED, (text_x - 20, text_y - 20, text_rect.width + 40, text_rect.height + 40 ), 0)
-        self._display_surf.blit(text, [text_x, text_y])
-        
-        winner_str = "Winner(s): " 
-        loser_str = "Loser(s): " 
+        winner_str = "Winner: " 
         for i in range(self.n_players):
             if i in range(self.n_players): 
-                if self.winners[i] == 0: loser_str += "Player " + str(i + 1) + " "
-                else: 
-                    winner_str += "Player " + str(i + 1) + " "
-        font = pygame.font.Font(None, 50)
+                if self.winners[i] == 1: winner_str += "Player " + str(i + 1)
+        font = pygame.font.Font(None, 300)
         winner_text = font.render(winner_str, True, self.LIMEGREEN)
-        loser_text = font.render(loser_str, True, self.LIMEGREEN)
-        self._display_surf.blit(winner_text, [int(self.windowWidth / 2 - winner_text.get_rect().width / 2), 200])
-        self._display_surf.blit(loser_text, [int(self.windowWidth / 2 - loser_text.get_rect().width / 2), 280])
+        self._display_surf.blit(winner_text, [int(self.windowWidth / 2 - winner_text.get_rect().width / 2), int(self.windowHeight / 2 - winner_text.get_rect().height / 2 - 100)])
 
         score_str = ""
         font = pygame.font.Font(None, 75)
         for i in range(self.n_players):
             score_str += "Player " + str(i + 1) + ": " + str(self.scores[i]) + "      "
         score_text = font.render(score_str, True, self.PINK)
-        self._display_surf.blit(score_text, [int(self.windowWidth / 2 - score_text.get_rect().width / 2), 100])
+        self._display_surf.blit(score_text, [int(self.windowWidth / 2 - score_text.get_rect().width / 2), 200])
         
         self._draw_button("Play Again", self.DARKTEAL, self.DARKTEALHOVER, self.on_reset, y=int(self.windowHeight / 2 + 100))
-        self._draw_button("Exit", self.DARKRED, self.DARKREDHOVER, self.set_newgame_true, y=int(self.windowHeight / 2 + 200))
+        self._draw_button("Exit", self.DARKRED, self.DARKREDHOVER, self.on_newgame, y=int(self.windowHeight / 2 + 200))
 
     def _draw_button(self, msg, init_color, hover_color, action, x=-1, y=-1, w=-1, h=-1):
         font = pygame.font.Font(None, 40)
